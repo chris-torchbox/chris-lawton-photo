46e4feeb033af774fc7b61ffd39e945a1524d32d
Chris Lawton <chris.lawton@gdsinternational.com>
2017-08-09 10:25:42 +0100

    the home page now utilises the home container, not component

--- src/js/components/main.js
+++ src/js/components/main.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import ScrollToTop from './scroll-to-top';
-import Home from './pages/home';
-// import Post from './pages/post';
 import Post from './../containers/post';
+import Home from './../containers/home';
 import NotFound from './../components/not-found';
 
 function Main() {
