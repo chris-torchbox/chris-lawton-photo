cc666d710e84f8f524a4b0341b10098c99c1993a
Chris Lawton <chris.lawton@gdsinternational.com>
2017-08-18 09:06:40 +0100

    changing overlay to be a self enclosing tag

--- src/js/components/posts-grid.js
+++ src/js/components/posts-grid.js
@@ -12,8 +12,8 @@ class PostsGrid extends Component {
     console.log(allCollectionsArray);
 
     return allCollectionsArray.map(collection =>
-        <div className="overlay"></div>
       <NavLink to={this.parseCollectionHref(collection.title)} key={collection.title} className="grid-item">
+        <div className="overlay" />
         {/*<img src={collection.cover_photo.urls.small} alt="" />*/}
         <img src="https://placehold.it/400x267" alt="" />
         <div className="grid-item__content">
