557905ceda834050fa8900629f8d9f3e48e6f3e6
Chris Lawton <chris.lawton@gdsinternational.com>
2017-08-18 09:06:21 +0100

    adding method to parse the collection url, which returns as a lower case string prefxied with a path - the path may change yet though, i'm undecided

--- src/js/components/posts-grid.js
+++ src/js/components/posts-grid.js
@@ -12,8 +12,8 @@ class PostsGrid extends Component {
     console.log(allCollectionsArray);
 
     return allCollectionsArray.map(collection =>
-      <NavLink to={collection.title} key={collection.title} className="grid-item">
         <div className="overlay"></div>
+      <NavLink to={this.parseCollectionHref(collection.title)} key={collection.title} className="grid-item">
         {/*<img src={collection.cover_photo.urls.small} alt="" />*/}
         <img src="https://placehold.it/400x267" alt="" />
         <div className="grid-item__content">
@@ -23,6 +23,10 @@ class PostsGrid extends Component {
         </div>
       </NavLink>,
     );
+  }
+
+  parseCollectionHref(href) {
+    return `/trips/${href.toLowerCase()}`;
   }
 
   render() {
