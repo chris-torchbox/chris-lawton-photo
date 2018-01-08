d04f37fe0d6b78993e1b204f654d596d47c99bfc
Chris Lawton <chris.lawton@torchbox.com>
2018-01-08 18:58:05 +0000

    indenting styled.js

--- src/js/components/styled.js
+++ src/js/components/styled.js
@@ -1,25 +1,32 @@
 import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
+import PostData from './../data/trips/bristol';
 
 export const Grid = styled.div`
-  display: grid;
-  grid-template-columns: repeat(3, 426px);
+    display: grid;
+    grid-template-columns: repeat(3, 426px);
 `;
 
 export const Card = styled(NavLink)`
-  color: white;
-`;
-
-export const Title = styled.h3`
-  font-weight: 700;
+    color: white;
+    position: relative;
 `;
 
 export const CardContent = styled.div`
-  display: flex;
+    align-items: center;
+    bottom: 20px;
+    display: flex;
+    position: absolute;
+    flex-direction: column;
+    width: 100%;
+`;
+
+export const Title = styled.h3`
+    font-weight: 700;
 `;
 
 export const Teaser = styled.p`
-
+    margin-bottom: 10px;
 `;
 
 export const Date = styled.p`
@@ -28,5 +35,6 @@ export const Date = styled.p`
 
 export const Hero = styled.div`
   height: 100vh;
-  background: lightgrey;
+  background: url(${PostData[0].hero}), no-repeat, center;
+  background-size: cover;
 `;
