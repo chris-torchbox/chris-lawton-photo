11886b92505a8b8405589e14425a34379951a4f1
Chris Lawton <chris.lawton@torchbox.com>
2018-01-08 19:17:30 +0000

    adding bristol + copenhagen json

---
+++ src/js/data/trips/copenhagen.js
@@ -0,0 +1,10 @@
+const postData = [
+  {
+    title: 'Copenhagen',
+    teaser: 'This is some intro text',
+    date: 'January 6th 2018',
+    hero: 'https://picsum.photos/2560/1440',
+  },
+];
+
+export default postData;
