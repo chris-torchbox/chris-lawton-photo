320025b0dfed5bdf654ae3768168ac1727a41eea
Chris Lawton <chris.lawton@torchbox.com>
2018-01-09 20:42:30 +0000

    adding fetch in post to grab json and pass response as state

--- src/js/components/pages/post.js
+++ src/js/components/pages/post.js
@@ -1,17 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Helmet from 'react-helmet';
-import { Hero } from './../styled';
+import { Hero, HeroHeading, HeroSubHeading } from './../styled';
 
-function Post(props) {
-  return (
-    <div>
-      <Helmet>
-        <title>{props.match.params.post} | Chris Lawton Photography</title>
-      </Helmet>
-      <Hero />
-      {props.match.params.post}
-    </div>
-  );
+class Post extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: true,
+      tripData: null,
+    };
+  }
+
+  componentDidMount() {
+    // const url = `./../../data/trips/.json`;
+    const url = `http://localhost:8080/src/js/data/trips/${this.props.match.params.post}.json`;
+
+    fetch(url)
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error('Bad response from server');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({
+          tripData: data,
+          isLoading: false,
+        });
+      });
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      // todo - add spinner
+      return null;
+    }
+    const post = this.state.tripData;
+
+    const divStyle = {
+      backgroundImage: `url('${post.hero}')`,
+    };
+
+    return (
+      <div>
+        <Helmet>
+          <title>{post.title} | Chris Lawton Photography</title>
+        </Helmet>
+        <Hero style={divStyle}>
+          <HeroHeading>{post.title}</HeroHeading>
+          <HeroSubHeading>{post.country}</HeroSubHeading>
+        </Hero>
+      </div>
+    );
+  }
 }
 
 export default Post;
