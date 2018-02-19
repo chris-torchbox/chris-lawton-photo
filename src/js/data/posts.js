7baba1f87fa4dbe26a1d2182497ed234d70c8501
Chris Lawton <chris.lawton@torchbox.com>
2018-02-19 19:47:59 +0000

    adding more posts

--- src/js/data/posts.js
+++ src/js/data/posts.js
@@ -1,19 +1,101 @@
 const postsData = [
   {
+    title: 'Bali',
+    slug: '/bali/',
+    date: 'January 6th 2018',
+    thumbnail: 'bali.jpg',
+  },
+  {
+    title: 'Barcelona',
+    slug: '/barcelona/',
+    date: 'January 6th 2018',
+    thumbnail: 'barcelona.jpg',
+  },
+  {
+    title: 'Berlin',
+    slug: '/berlin/',
+    date: 'January 6th 2018',
+    thumbnail: 'berlin.jpg',
+  },
+  {
     title: 'Bristol',
     slug: '/bristol/',
     date: 'January 6th 2018',
+    thumbnail: 'bristol.jpg',
   },
   {
     title: 'Copenhagen',
     slug: '/copenhagen/',
     date: 'March 18th 2016',
+    thumbnail: 'copenhagen.jpg',
+  },
+  {
+    title: 'Grand Canyon',
+    slug: '/grand-canyon/',
+    date: 'April 20th 2017',
+    thumbnail: 'grand-canyon.jpg',
   },
   {
     title: 'Hereford',
     slug: '/hereford/',
     date: 'April 20th 2017',
+    thumbnail: 'hereford.jpg',
   },
+  {
+    title: 'Joch',
+    slug: '/joch/',
+    date: 'April 20th 2017',
+    thumbnail: 'joch.jpg',
+  },
+  {
+    title: 'Lacanau',
+    slug: '/lacanau/',
+    date: 'April 20th 2017',
+    thumbnail: 'lacanau.jpg',
+  },
+  {
+    title: 'NYC',
+    slug: '/nyc/',
+    date: 'April 20th 2017',
+    thumbnail: 'nyc.jpg',
+  },
+  {
+    title: 'Paris',
+    slug: '/paris/',
+    date: 'April 20th 2017',
+    thumbnail: 'paris.jpg',
+  },
+  {
+    title: 'Reykjavik',
+    slug: '/reykjavik/',
+    date: 'April 20th 2017',
+    thumbnail: 'reykjavik.jpg',
+  },
+  {
+    title: 'Santorini',
+    slug: '/santorini/',
+    date: 'April 20th 2017',
+    thumbnail: 'santorini.jpg',
+  },
+  {
+    title: 'San Francisco',
+    slug: '/san-francisco/',
+    date: 'April 20th 2017',
+    thumbnail: 'san-francisco.jpg',
+  },
+  {
+    title: 'Monterey',
+    slug: '/monterey/',
+    date: 'April 20th 2017',
+    thumbnail: 'monterey.jpg',
+  },
+  {
+    title: 'Yosemite',
+    slug: '/yosemite/',
+    date: 'April 20th 2017',
+    thumbnail: 'yosemite.jpg',
+  },
+
 ];
 
 export default postsData;
