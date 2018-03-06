c97139711298a4a0963db00ee495c6f5c8a291dd
Chris Lawton <chris.lawton@torchbox.com>
2018-03-06 20:48:20 +0000

    lint indent

--- src/js/components/styled/card-content.js
+++ src/js/components/styled/card-content.js
@@ -2,13 +2,13 @@ import styled from 'styled-components';
 import { mq } from './media-queries';
 
 export const CardContent = styled.div`
-align-items: center;
-bottom: 20px;
-display: flex;
-position: absolute;
-flex-direction: column;
-width: 100%;
-${mq.handheld`bottom: 35px;`}
+    align-items: center;
+    bottom: 20px;
+    display: flex;
+    position: absolute;
+    flex-direction: column;
+    width: 100%;
+    ${mq.handheld`bottom: 35px;`}
 `;
 
 export default CardContent;
--- src/js/components/styled/date.js
+++ src/js/components/styled/date.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
 export const Date = styled.p`
-font-family: 'Lato', serif;
-font-size: 12px;
+    font-family: 'Lato', serif;
+    font-size: 12px;
 `;
