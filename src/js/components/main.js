4a08229e810afa41ab75d49bb52df8740de430b7
Chris Lawton <chris.lawton@torchbox.com>
2018-03-06 20:49:51 +0000

    main component can be an explicit return

--- src/js/components/main.js
+++ src/js/components/main.js
@@ -5,18 +5,16 @@ import Home from './pages/home';
 import Post from './pages/post';
 import NotFound from './../components/not-found';
 
-function Main() {
-  return (
-    <div>
-      <ScrollToTop>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/:post/" component={Post} />
-          <Route component={NotFound} />
-        </Switch>
-      </ScrollToTop>
-    </div>
-  );
-}
+const Main = () => (
+  <div>
+    <ScrollToTop>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/:post/" component={Post} />
+        <Route component={NotFound} />
+      </Switch>
+    </ScrollToTop>
+  </div>
+);
 
 export default Main;
