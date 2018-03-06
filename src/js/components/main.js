bf57a1887d0a2665f4e26443caf39d586c2112f5
Chris Lawton <chris.lawton@torchbox.com>
2018-03-06 21:31:09 +0000

    adding gear page

--- src/js/components/main.js
+++ src/js/components/main.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import ScrollToTop from './scroll-to-top';
 import Home from './pages/home';
 import Post from './pages/post';
+import Gear from './pages/gear';
 import NotFound from './../components/not-found';
 
 const Main = () => (
@@ -10,6 +11,7 @@ const Main = () => (
     <ScrollToTop>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/gear" component={Gear} />
         <Route exact path="/:post/" component={Post} />
         <Route component={NotFound} />
       </Switch>
---
+++ src/js/components/pages/gear.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { TextContainer } from './../styled/container';
+
+const Gear = () => (
+  <div>
+    <TextContainer>
+      <h2>Gear</h2>
+      <p>FujiFilm X100F</p>
+      <p>SanDisk</p>
+      <p>Manfrotto</p>
+      <p>LensCap</p>
+      <p>Hyperion Strap</p>
+      <p>Case</p>
+      <p>Lightroom Classic CC</p>
+      <p>Hard Drive</p>
+    </TextContainer>
+  </div>
+);
+
+export default Gear;
