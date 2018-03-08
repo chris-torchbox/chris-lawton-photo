b6c7179eb35c5e0de5950d885053c0a19d2e4ccc
Chris Lawton <chris.lawton@torchbox.com>
2018-03-08 21:25:37 +0000

    lint, indent

--- src/js/app.js
+++ src/js/app.js
@@ -16,30 +16,31 @@ const description = '';
 const hero = '';
 
 ReactDOM.render(
-    <div>
-      <Helmet>
-        <title>{title}</title>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width" />
-        <meta name="robots" content="noodp" />
-        <meta property="fb:app_id" content="" />
-        <meta property="og:locale" content="en_GB" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={description} />
-        <meta property="og:url" content="https://chrislawton.co.uk/" />
-        <meta property="og:site_name" content="Chris Lawton Photography" />
-        <meta property="og:image" content={hero} />
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:description" content={description} />
-        <meta name="twitter:title" content={title} />
-        <meta name="twitter:image" content={hero} />
-      </Helmet>
-      <BrowserRouter>
-        <div>
-          <Header />
-          <Main />
-        </div>
-      </BrowserRouter>
-    </div>
-  , document.querySelector('.container'));
+  <div>
+    <Helmet>
+      <title>{title}</title>
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width" />
+      <meta name="robots" content="noodp" />
+      <meta property="fb:app_id" content="" />
+      <meta property="og:locale" content="en_GB" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:url" content="https://chrislawton.co.uk/" />
+      <meta property="og:site_name" content="Chris Lawton Photography" />
+      <meta property="og:image" content={hero} />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:image" content={hero} />
+    </Helmet>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Main />
+      </div>
+    </BrowserRouter>
+  </div>
+  , document.querySelector('.container'),
+);
