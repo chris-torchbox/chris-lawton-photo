9f7233e53c07a989e834405fcba2c79f4d632462
Chris Lawton <chris.lawton@torchbox.com>
2018-09-10 18:37:03 +0100

    remove insta unsplash svgs

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -8,10 +8,10 @@ const Header = () => (
     <SiteTitle to="/">Chris Lawton</SiteTitle>
     <div>
       <SocialLink target="_blank" href="https://instagram.com/chris.lawton" aria-label="Link to Instagram">
-        <svg fill="#9B9999" width="25px" height="25px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 56.7 56.7"><path d="M28.2 16.7c-7 0-12.8 5.7-12.8 12.8s5.7 12.8 12.8 12.8S41 36.5 41 29.5s-5.8-12.8-12.8-12.8zm0 21c-4.5 0-8.2-3.7-8.2-8.2s3.7-8.2 8.2-8.2 8.2 3.7 8.2 8.2-3.7 8.2-8.2 8.2z" /><circle cx="41.5" cy="16.4" r="2.9" /><path d="M49 8.9c-2.6-2.7-6.3-4.1-10.5-4.1H17.9c-8.7 0-14.5 5.8-14.5 14.5v20.5c0 4.3 1.4 8 4.2 10.7 2.7 2.6 6.3 3.9 10.4 3.9h20.4c4.3 0 7.9-1.4 10.5-3.9 2.7-2.6 4.1-6.3 4.1-10.6V19.3c0-4.2-1.4-7.8-4-10.4zm-.4 31c0 3.1-1.1 5.6-2.9 7.3s-4.3 2.6-7.3 2.6H18c-3 0-5.5-.9-7.3-2.6C8.9 45.4 8 42.9 8 39.8V19.3c0-3 .9-5.5 2.7-7.3 1.7-1.7 4.3-2.6 7.3-2.6h20.6c3 0 5.5.9 7.3 2.7 1.7 1.8 2.7 4.3 2.7 7.2v20.6z" /></svg>
+        Instagram
       </SocialLink>
       <SocialLink target="_blank" href="https://unsplash.com/@chrislawton" aria-label="Link to Unsplash">
-        <svg height="512" viewBox="0 0 32 32" width="512" xmlns="http://www.w3.org/2000/svg"><path d="m20.8 18.1c0 2.7-2.2 4.8-4.8 4.8s-4.8-2.1-4.8-4.8 2.2-4.8 4.8-4.8c2.7.1 4.8 2.2 4.8 4.8zm11.2-7.4v14.9c0 2.3-1.9 4.3-4.3 4.3h-23.4c-2.4 0-4.3-1.9-4.3-4.3v-15c0-2.3 1.9-4.3 4.3-4.3h3.7l.8-2.3c.4-1.1 1.7-2 2.9-2h8.6c1.2 0 2.5.9 2.9 2l.8 2.4h3.7c2.4 0 4.3 1.9 4.3 4.3zm-8.6 7.5c0-4.1-3.3-7.5-7.5-7.5-4.1 0-7.5 3.4-7.5 7.5s3.3 7.5 7.5 7.5c4.2-.1 7.5-3.4 7.5-7.5z" /></svg>
+        Unsplash
       </SocialLink>
     </div>
   </HeaderMain>
